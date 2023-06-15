a7d849409df16061e8db830799c587ab8f469238
Samrat Biswas <102436136+samratpro@users.noreply.github.com>
2023-06-16 04:31:17 +0600

    Update 5. admin.py

--- 5. admin.py
+++ 5. admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import *                # or from .models import "ModuleName" 
 
 # Register your models here.
-admin.site.register("Module_Name") 
+admin.site.register(Module_Name) 
 
 # Changing the Django Admin Header Text
 admin.site.site_header = 'Project Name'    
