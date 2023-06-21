f741b885f924aec014b7aa950f52420eab5295a2
Samrat Biswas <102436136+samratpro@users.noreply.github.com>
2023-06-22 05:26:15 +0600

    Update views_list_single_update_delete_query_.py

--- Model/views_list_single_update_delete_query_.py
+++ Model/views_list_single_update_delete_query_.py
@@ -8,31 +8,31 @@ def AllDataShow(request):
     context = {'all_data':all_data}
     return render(request, template, context=context)
 
-
-
+# Viewing Single Data .....................................
+def single_data(request, data_id):
+    template = "single_data.html"
+    sigle_data = WesiteModel.objects.get(pk=data_id)
+    context = {'sigle_data': sigle_data,'data_id': data_id}
+    return render(request, template, context)  
 
 
 
 # Update Data ...................................................
-def update_data(request, website_id):
+def update_data(request, data_id):
     template = "update_data.html"
-    website = WesiteModel.objects.get(pk=website_id)
+    data = WesiteModel.objects.get(pk=data_id)
     if request.method == "POST":
         update_form = WebsiteForms(request.POST)
         if update_form.is_valid():
-            website.website_name = update_form.cleaned_data['website_name']
-            website.website_url = update_form.cleaned_data['website_url']
-            website.username = update_form.cleaned_data['username']
-            website.app_pass = update_form.cleaned_data['app_pass']
-            website.save()
+            data.data_name = update_form.cleaned_data['data_name']
+            data.data_url = update_form.cleaned_data['data_url']
+            data.save()
             return redirect('/website')
     else:
         update_form = WebsiteForms(initial={
-            'website_name': website.website_name,
-            'website_url': website.website_url,
-            'username': website.username,
-            'app_pass': website.app_pass
+            'data_name': website.data_name,
+            'data_url': website.data_url,
         })
-    context = {'update_form': update_form,'website_id': website_id}
+    context = {'update_form': update_form,'data_id': data_id}
     return render(request, template, context)
 
