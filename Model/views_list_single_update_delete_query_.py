7017be147caa0d2e8e3e3728e2ad45d107796620
Samrat Biswas <102436136+samratpro@users.noreply.github.com>
2023-06-22 02:40:44 +0600

    Update views_list_single_update_delete_query_.py

--- Model/views_list_single_update_delete_query_.py
+++ Model/views_list_single_update_delete_query_.py
@@ -1,8 +1,38 @@
 # General Query All Data
 from .models import *
-def DataShow(request):
+
+# Showing all data, like post .....................................................
+def AllDataShow(request):
     all_data = WesiteModel.objects.all()
-    template = 'data__show__temp__.html'
+    template = 'all_data_show.html'
     context = {'all_data':all_data}
     return render(request, template, context=context)
+
+
+
+
+
+
+# Update Data ...................................................
+def update_website(request, website_id):
+    template = "update_website.html"
+    website = WesiteModel.objects.get(pk=website_id)
+    if request.method == "POST":
+        update_form = WebsiteForms(request.POST)
+        if update_form.is_valid():
+            website.website_name = update_form.cleaned_data['website_name']
+            website.website_url = update_form.cleaned_data['website_url']
+            website.username = update_form.cleaned_data['username']
+            website.app_pass = update_form.cleaned_data['app_pass']
+            website.save()
+            return redirect('/website')
+    else:
+        update_form = WebsiteForms(initial={
+            'website_name': website.website_name,
+            'website_url': website.website_url,
+            'username': website.username,
+            'app_pass': website.app_pass
+        })
+    context = {'update_form': update_form,'website_id': website_id}
+    return render(request, template, context)
 
