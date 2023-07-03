6557e1e5d2f2bf96af4413bfbe4de131d4fada74
Samrat Biswas <102436136+samratpro@users.noreply.github.com>
2023-07-04 04:34:38 +0600

    Update 3. url-mapping.py

--- 3. url-mapping.py
+++ 3. url-mapping.py
@@ -1,6 +1,8 @@
 # URL Mapping in Project URL file-------------------
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', include('NewApp.urls')),
@@ -19,5 +21,6 @@ urlpatterns = [
     path('article', views.article, name='article'),  
     path('about', views.about, name='about'),  
 ]
-# Home is function name here...
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 # >>> 404.html and Debug is false for 404 page
