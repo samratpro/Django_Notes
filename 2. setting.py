0e65ce9d0108ba52c3b808b74182cb9a28f9880c
Samrat Biswas <102436136+samratpro@users.noreply.github.com>
2023-07-04 04:31:02 +0600

    Update 2. setting.py

--- 2. setting.py
+++ 2. setting.py
@@ -12,15 +12,20 @@ DIRS': [BASE_DIR / 'templates'], # Probbaly 59 line
 
 
 # Static File Connection..   # Probbaly 123 line
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
 
 # This is for Hosted server and When Debug is False after Hosting Django App in Domain
 STATIC_ROOT = BASE_DIR / "staticfiles"
-# python manage.py collectstatic
-# >>> Collect Admin CSS files from " staticfiles " and paste it in " static " Folder
+python manage.py collectstatic
+# >>> Now configure it in Project urls.py
+from django.conf import settings
+from django.conf.urls.static import static
+urlpatterns = []
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
 # >>> If Debug is True then admin CSS won't work in the local Server
 # >>> 404.html and Debug is false for 404 page
 
