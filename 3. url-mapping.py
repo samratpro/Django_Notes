cacf66c8b1caf427dc045814a6e81284d26db2b2
Samrat Biswas <102436136+samratpro@users.noreply.github.com>
2023-08-03 15:40:10 +0600

    Update 3. url-mapping.py

--- 3. url-mapping.py
+++ 3. url-mapping.py
@@ -5,7 +5,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', include('NewApp.urls')),
+    path('admin/', admin.site.urls),
+    path('', include('NewApp.urls'))
 ]  # NewApp is App name here...
 
 
