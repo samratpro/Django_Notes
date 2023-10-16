1da5e78a518f09f02f1894336b25ee5d06f40b6d
Samrat Biswas <102436136+samratpro@users.noreply.github.com>
2023-10-17 03:58:58 +0600

    Update 1. basic_setup.py

--- 8. REST Framework/1. basic_setup.py
+++ 8. REST Framework/1. basic_setup.py
@@ -1,1 +1,7 @@
-test
+# Documentation : https://www.django-rest-framework.org/
+pip install djangorestframework
+
+# add a new app in setting with name of "rest_framework"
+INSTALLED_APPS = [
+    'rest_framework'
+]
