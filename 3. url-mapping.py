a069f1b638aad2c0c5801990b36a6324fbde86a2
Samrat Biswas <102436136+samratpro@users.noreply.github.com>
2023-10-17 00:47:30 +0600

    Update 3. url-mapping.py

--- 3. url-mapping.py
+++ 3. url-mapping.py
@@ -6,7 +6,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('NewApp.urls'))
+    path('', include('NewApp.urls'))   # Must Have to create urls.py in App folder >> then at least one template render function in views.py 
 ]  # NewApp is App name here...
 
 
