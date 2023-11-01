2ec0f7e67a794c2af9fd8151c46391a2a93453ac
Samrat Biswas <102436136+samratpro@users.noreply.github.com>
2023-11-02 05:50:45 +0600

    Update models.py

--- 3. User Authentication/Email Verification/models.py
+++ 3. User Authentication/Email Verification/models.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.models import AbstractUser, Group, Permission
 from django.db import models
 
-class AppUser(AbstractUser):
+class AppUser(AbstractUser):  # It is a custom user type where any fields can be modify, Like I've added image here
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     email = models.EmailField(unique=True)
