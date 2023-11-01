3eaedf9ce7cd38f7fa4d1db5e44d3cc45aa20522
Samrat Biswas <102436136+samratpro@users.noreply.github.com>
2023-11-02 05:51:32 +0600

    Create admin.py

---
+++ 3. User Authentication/Email Verification/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import AppUser  # Replace with your custom user model
+
+class CustomUserAdmin(UserAdmin):
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        ('Personal info', {'fields': ('email', 'first_name', 'last_name', 'profile_image', 'activation_code')}),   # profile_image is custom field here
+        ('Permissions', {'fields': ('is_active', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+
+admin.site.register(AppUser, CustomUserAdmin)
