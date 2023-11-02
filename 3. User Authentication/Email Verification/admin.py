4382f5cf5eaca3fe2fb9e2ad4ffe814656c9b0a4
Samrat Biswas <102436136+samratpro@users.noreply.github.com>
2023-11-03 00:04:11 +0600

    Update admin.py

--- 3. User Authentication/Email Verification/admin.py
+++ 3. User Authentication/Email Verification/admin.py
@@ -9,7 +9,7 @@ class CustomUserAdmin(UserAdmin):
     fieldsets = (
         (None, {'fields': ('username', 'password')}),
         ('Personal info', {'fields': ('email', 'first_name', 'last_name', 'profile_image', 'activation_code')}),   # profile_image is custom field here
-        ('Permissions', {'fields': ('is_active', 'groups', 'user_permissions')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
         ('Important dates', {'fields': ('last_login', 'date_joined')}),
     )
 
