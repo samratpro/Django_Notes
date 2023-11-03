d7e0d48b21a6a80cb48b33d59659bcfdaa61e2e1
Samrat Biswas <102436136+samratpro@users.noreply.github.com>
2023-11-04 04:58:16 +0600

    Update urls.py

--- 3. User Authentication/Email Verification/urls.py
+++ 3. User Authentication/Email Verification/urls.py
@@ -5,10 +5,15 @@ from django.conf.urls.static import static
 
 
 urlpatterns = [
-    path('register/', views.register, name='register'),
-    path('activate/<str:activation_code>/', views.activate_account, name='activate_account'),
-    path('login/', views.login, name='login'),
-    path('logout/', views.logout, name='logout'),
+    
+    path('login/', views.login, name='login'),   
+    path('logout', views.logout, name='logout'),  
+     
+    path('register', views.register, name='register'),   
+    path('activate/<str:activation_code>/', views.activate_account, name='activate_account'),  
+    
+    path('forget_password/', views.forget_password, name='forget_password'),
+    path('forget_password/confirm/<str:forget_code>/', views.forget_password_confirm, name='forget_password_confirm'),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) 
