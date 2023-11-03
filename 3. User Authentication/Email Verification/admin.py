8bdec54a5968e8a6b1616f5d03a2146aa81e54cd
Samrat Biswas <102436136+samratpro@users.noreply.github.com>
2023-11-04 05:02:12 +0600

    Update admin.py

--- 3. User Authentication/Email Verification/admin.py
+++ 3. User Authentication/Email Verification/admin.py
@@ -2,15 +2,15 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import AppUser  # Replace with your custom user model
 
-
-# This for viewing custom user in admin panel
-
 class CustomUserAdmin(UserAdmin):
     fieldsets = (
         (None, {'fields': ('username', 'password')}),
-        ('Personal info', {'fields': ('email', 'first_name', 'last_name', 'profile_image', 'activation_code')}),   # profile_image is custom field here
+        ('Personal info', {'fields': ('email', 'first_name', 'last_name', 'profile_image', 'activation_code', 'credit', 'password_reset_code')}),
         ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
         ('Important dates', {'fields': ('last_login', 'date_joined')}),
     )
 
 admin.site.register(AppUser, CustomUserAdmin)
+
+
+# 'profile_image', 'activation_code', 'credit', 'password_reset_code'  THESE ARE CUSTOM FIELDS HERE
