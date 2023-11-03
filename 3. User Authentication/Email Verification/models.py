941ab781b39e19f30c7d0eeed9a42cfdb88b4050
Samrat Biswas <102436136+samratpro@users.noreply.github.com>
2023-11-04 04:58:47 +0600

    Update models.py

--- 3. User Authentication/Email Verification/models.py
+++ 3. User Authentication/Email Verification/models.py
@@ -1,12 +1,14 @@
 from django.contrib.auth.models import AbstractUser, Group, Permission
 from django.db import models
 
-class AppUser(AbstractUser):  # It is a custom user type where any fields can be modify, Like I've added image here
+class AppUser(AbstractUser):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     email = models.EmailField(unique=True)
-    activation_code = models.CharField(max_length=30, blank=True, null=True)
+    activation_code = models.CharField(max_length=50, blank=True, null=True)
+    password_reset_code = models.CharField(max_length=50, blank=True, null=True)
     profile_image = models.ImageField(upload_to='profile_images/', blank=True, null=True)
+    credit = models.IntegerField(default=0)
     is_active = models.BooleanField(default=False)
     
     # Specify related_name for groups and user_permissions to resolve the clash
@@ -17,4 +19,4 @@ class AppUser(AbstractUser):  # It is a custom user type where any fields can be
         self.is_active = True
         self.activation_code = ''
         self.save()
-
+        
