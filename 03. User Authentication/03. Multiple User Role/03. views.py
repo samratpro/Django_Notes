e7abf9f720d26791cfeb29ebfcc44ae5b7b0944f
Samrat Biswas <102436136+samratpro@users.noreply.github.com>
2023-11-12 02:53:13 +0600

    Update 03. views.py

--- 03. User Authentication/03. Multiple User Role/03. views.py
+++ 03. User Authentication/03. Multiple User Role/03. views.py
@@ -3,7 +3,7 @@ from django.contrib.auth import authenticate, login
 from .models import AppUser, Task, TaskPermission
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import get_object_or_404, redirect
-from .models import UserProfile, Teacher, Student
+from .models import AppUser, Teacher, Student
 
 def login_view(request):
     if request.method == 'POST':
