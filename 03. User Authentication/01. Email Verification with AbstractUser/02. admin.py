230d80788598e0fcfa2f3881c55536ea79145936
Samrat Biswas <102436136+samratpro@users.noreply.github.com>
2023-11-13 06:43:15 +0600

    Update 02. admin.py

--- 03. User Authentication/01. Email Verification with AbstractUser/02. admin.py
+++ 03. User Authentication/01. Email Verification with AbstractUser/02. admin.py
@@ -1,31 +1,33 @@
 from django.contrib import admin
-# from django.utils.translation import gettext_lazy as _
-# from django.contrib.auth.admin import UserAdmin
 from .models import AppUser  # Replace with your custom user model
 # from .models import Customer
-
-# @admin.register(AppUser)
-# class UserAdmin(UserAdmin):
-#     """Define admin model for custom User model with no email field."""
-
-#     fieldsets = (
-#         (None, {'fields': ('email', 'password')}),
-#         (_('Personal info'), {'fields': ('first_name', 'last_name', 'activation_code', 'password_reset_code', 'profile_image', 'credit')}),
-#         (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser','groups', 'user_permissions')}),
-#         (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
-#         )
-#     add_fieldsets = ((None, {'classes': ('wide',),'fields': ('email', 'password1', 'password2'),}),)
-#     list_display = ('email', 'first_name', 'last_name', 'is_staff')
-#     search_fields = ('email', 'first_name', 'last_name')
-#     ordering = ('email',)
 
 
 admin.site.register(AppUser)
 # admin.site.register(Customer)
+
 # Changing the Django Admin Header Text
 admin.site.site_header = 'AI Writing Project'   
 
-# 'profile_image', 'activation_code', 'credit', 'password_reset_code'  THESE ARE CUSTOM FIELDS HERE
 
+'''
+# from django.utils.translation import gettext_lazy as _
+# from django.contrib.auth.admin import UserAdmin
 
+@admin.register(AppUser)
+class UserAdmin(UserAdmin):
+    """Define admin model for custom User model with no email field."""
 
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (_('Personal info'), {'fields': ('first_name', 'last_name', 'activation_code', 'password_reset_code', 'profile_image', 'credit')}),
+        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser','groups', 'user_permissions')}),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+        )
+    add_fieldsets = ((None, {'classes': ('wide',),'fields': ('email', 'password1', 'password2'),}),)
+    list_display = ('email', 'first_name', 'last_name', 'is_staff')
+    search_fields = ('email', 'first_name', 'last_name')
+    ordering = ('email',)
+
+    'profile_image', 'activation_code', 'credit', 'password_reset_code'  THESE ARE CUSTOM FIELDS HERE
+'''
