13af503504599a0b9ba2f2141063c47082c45092
Samrat Biswas <102436136+samratpro@users.noreply.github.com>
2023-11-18 01:16:15 +0600

    Update 02. admin.py

--- 03. User Authentication/01. Email Verification with AbstractUser/02. admin.py
+++ 03. User Authentication/01. Email Verification with AbstractUser/02. admin.py
@@ -10,24 +10,33 @@ admin.site.register(AppUser)
 admin.site.site_header = 'AI Writing Project'   
 
 
-'''
-# from django.utils.translation import gettext_lazy as _
-# from django.contrib.auth.admin import UserAdmin
-
-@admin.register(AppUser)
-class UserAdmin(UserAdmin):
-    """Define admin model for custom User model with no email field."""
+# Custom Admin View for User Management
+# Included password Reset Fields
+from django.contrib.auth.admin import UserAdmin
+class AppUserAdmin(UserAdmin):
+    model = AppUser
+    list_display = ('email', 'first_name', 'last_name', 'is_active', 'is_staff', 'is_superuser')
+    search_fields = ('email', 'first_name', 'last_name')
+    ordering = ('email',)
+    filter_horizontal = ('groups', 'user_permissions')
 
     fieldsets = (
         (None, {'fields': ('email', 'password')}),
-        (_('Personal info'), {'fields': ('first_name', 'last_name', 'activation_code', 'password_reset_code', 'profile_image', 'credit')}),
-        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser','groups', 'user_permissions')}),
-        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
-        )
-    add_fieldsets = ((None, {'classes': ('wide',),'fields': ('email', 'password1', 'password2'),}),)
-    list_display = ('email', 'first_name', 'last_name', 'is_staff')
-    search_fields = ('email', 'first_name', 'last_name')
-    ordering = ('email',)
+        ('Personal info', {'fields': ('first_name', 'last_name')}),
+        ('Permissions', {
+            'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions'),
+        }),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+        ('Custom Fields', {'fields': ('activation_code', 'password_reset_code', 'profile_image', 'credit')}),
+    )
 
-    'profile_image', 'activation_code', 'credit', 'password_reset_code'  THESE ARE CUSTOM FIELDS HERE
-'''
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2'),
+        }),
+    )
+
+
+admin.site.register(AppUser, AppUserAdmin)
+
