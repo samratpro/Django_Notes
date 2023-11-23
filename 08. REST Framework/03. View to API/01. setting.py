9d74da2ef16f68612fac25b3ba2a8e226323968e
Samrat Biswas <102436136+samratpro@users.noreply.github.com>
2023-11-24 04:04:55 +0600

    Update 01. setting.py

--- 08. REST Framework/03. View to API/01. setting.py
+++ 08. REST Framework/03. View to API/01. setting.py
@@ -1,2 +1,31 @@
 # pip install djangorestframework
 # pip install djangorestframework-simplejwt
+
+
+
+
+# Add 'rest_framework', and 'rest_framework_simplejwt' to INSTALLED_APPS
+INSTALLED_APPS = [
+    'rest_framework',
+    'rest_framework_simplejwt',
+]
+
+
+
+
+# Add JWT authentication settings
+from datetime import timedelta
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+}
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),  # Set token lifetime as needed
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'ROTATE_REFRESH_TOKENS': False,
+    'BLACKLIST_AFTER_ROTATION': False,
+    'UPDATE_LAST_LOGIN': False,
+}
