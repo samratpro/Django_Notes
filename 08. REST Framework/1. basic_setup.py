5a7b8a6a1aa2c8298acf104ec94e0bb1cf24d489
Samrat Biswas <102436136+samratpro@users.noreply.github.com>
2023-11-24 03:50:20 +0600

    Delete 08. REST Framework/1. basic_setup.py

--- 08. REST Framework/1. basic_setup.py
+++
@@ -1,7 +0,0 @@
-# Documentation : https://www.django-rest-framework.org/
-pip install djangorestframework
-
-# add a new app in setting with name of "rest_framework"
-INSTALLED_APPS = [
-    'rest_framework'
-]
