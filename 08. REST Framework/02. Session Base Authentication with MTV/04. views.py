4e88e87495d2e71296629f400e455228e6ec2f0c
Samrat Biswas <102436136+samratpro@users.noreply.github.com>
2023-11-25 06:22:27 +0600

    Create 04. views.py

---
+++ 08. REST Framework/02. Session Base Authentication with MTV/04. views.py
@@ -0,0 +1,17 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import GenerateTextSerializer
+
+
+class GenerateTextView(APIView):
+    def post(self, request):
+        print(request.user)
+        if request.user.is_authenticated:
+            serializer = GenerateTextSerializer(data=request.data)
+            if serializer.is_valid():
+                input_text = serializer.validated_data['input_text']
+                generated_text = f"Generated text for '{input_text}'"
+                return Response({'generated_text': generated_text}, status=status.HTTP_200_OK)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
