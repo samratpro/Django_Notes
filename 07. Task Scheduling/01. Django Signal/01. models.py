7ded0e3879a7e5bd586306b34457b497a2b97164
Samrat Biswas <102436136+samratpro@users.noreply.github.com>
2023-12-07 06:03:24 +0600

    Delete 07. Task Scheduling/01. Django Signal directory

--- 07. Task Scheduling/01. Django Signal/01. models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-class InfoBulkModel(models.Model):
-    user = models.CharField(max_length=100)
-    keyword = models.CharField(max_length=100)
-    status = models.CharField(max_length=100)
