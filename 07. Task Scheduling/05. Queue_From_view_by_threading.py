4aac32de7dccf97ad7503237746b6a8630474d4c
Samrat Biswas <102436136+samratpro@users.noreply.github.com>
2023-12-07 06:09:03 +0600

    Create 05. Queue_From_view_by_threading.py

---
+++ 07. Task Scheduling/05. Queue_From_view_by_threading.py
@@ -0,0 +1,48 @@
+#views.py
+from .task import *
+import threading
+scheduler_thread = None  
+def bulkpost(request):
+    template = 'bulkpost.html'
+    keyword_pending = BulkKeywordModel.objects.filter(status='Pending')
+    context = {'keyword_pending': keyword_pending}
+
+    if request.method == 'POST':
+        keyword_list = request.POST.get('keyword_list')
+        keywords = keyword_list.split('\n')
+
+        for keyword in keywords:
+            keyword = keyword.strip()
+            if keyword:
+                BulkKeywordModel.objects.create(name=keyword, status='Pending')
+
+        global scheduler_thread
+        if scheduler_thread is None or not scheduler_thread.is_alive():
+            # Start the task scheduler in a separate thread
+            scheduler_thread = threading.Thread(target=BulkDatasJob)      #  We can also input BulkDatasJob function's argument
+            # scheduler_thread = threading.Thread(target=BulkDatasJob, args=('arg',)) 
+            scheduler_thread.start()
+        return redirect('bulkpost')
+    
+    return render(request, template, context=context)
+
+# Models.py
+from django.db import models
+class InfoBulkModel(models.Model):
+    user = models.CharField(max_length=100)
+    keyword = models.CharField(max_length=100)
+    status = models.CharField(max_length=100)
+
+
+# task.py
+from .models import *
+from time import sleep
+
+def BulkDataJob():
+    pending_datas = BulkDataModel.objects.filter(status='Pending')
+    for data in pending_datas:
+        sleep(10)
+        data.content = 'Content'
+        data.status = 'Completed'
+        data.save()
+
