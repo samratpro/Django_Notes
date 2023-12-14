ebe8872cb1b0b8ca400ebfd5b5622e8dfbe0d9c8
Samrat Biswas <102436136+samratpro@users.noreply.github.com>
2023-12-15 05:58:21 +0600

    Update admin.py

--- 05. Models with CRUD/admin.py
+++ 05. Models with CRUD/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
 from .models import *                # or from .models import "ModuleName" 
 
+
+# Add the field to make read-only
+class ApiListAdmin(admin.ModelAdmin):
+    readonly_fields = ('filled_quota',)  
+admin.site.register(ApiList, ApiListAdmin)
+
+
 # Register your models here.
 admin.site.register(Module_Name) 
 
