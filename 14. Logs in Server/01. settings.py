42d9ef70d1dc0fee583e3506e05c4dc91ea26112
Samrat Biswas <102436136+samratpro@users.noreply.github.com>
2023-12-16 02:27:16 +0600

    Create 01. settings.py

---
+++ 14. Logs in Server/01. settings.py
@@ -0,0 +1,55 @@
+
+
+MIDDLEWARE = [
+    # ...
+    'your_app.middleware.RequestLoggingMiddleware',
+    # Make sure it comes before 'django.middleware.common.CommonMiddleware'
+    'django.middleware.common.CommonMiddleware',
+    # ...
+]
+
+
+
+
+# Django LOGGING in File
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': '/www/wwwroot/AI-SaaS/logs/django.log',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+    },
+}
+
+# or
+
+
+# Django LOGGING in Console
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django.db.backends': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+            'propagate': False,
+        },
+    },
+}
