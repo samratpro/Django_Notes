d30c9b275280656035bb5ed6b3ab9ed286a388a4
Samrat Biswas <102436136+samratpro@users.noreply.github.com>
2023-12-17 04:28:49 +0600

    Update 02. Logger_instead_print_function.py

--- 14. Logs in Server/02. Logger_instead_print_function.py
+++ 14. Logs in Server/02. Logger_instead_print_function.py
@@ -1,1 +1,14 @@
+# Using logger instead of print function, cause live server can't show data like VS Code or Pycharm terminal
 
+import logging
+# Get an instance of a logger
+logger = logging.getLogger(__name__)
+
+
+def your_view_function(request):
+    if condition:
+        logger.error('An error occurred')  # Example of logging an error
+        # Your error handling code
+    else:
+        logger.info('This is bulk info posting page, testing for Python logger')
+        return render(request, template, context=context)
