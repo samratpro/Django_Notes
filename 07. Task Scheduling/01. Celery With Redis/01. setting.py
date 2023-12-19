3759814481d9ce7349960e28b1e71d5f8f5c3571
Samrat Biswas <102436136+samratpro@users.noreply.github.com>
2023-12-20 05:23:16 +0600

    Create 01. setting.py

---
+++ 07. Task Scheduling/01. Celery With Redis/01. setting.py
@@ -0,0 +1,11 @@
+# Install Celery
+pip install celery
+
+# Install Redis (if not installed)
+# Windows, Download : https://www.memurai.com/
+# Macos, Download   : https://redis.io/docs/install/install-redis/install-redis-on-mac-os/
+# For Ubuntu
+sudo apt update
+sudo apt install redis-server
+
+# For other systems, refer to the Redis installation guide
