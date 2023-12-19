e1f9be82568f60c0142d11df35cad3555abb5acf
Samrat Biswas <102436136+samratpro@users.noreply.github.com>
2023-12-20 05:35:39 +0600

    Update 01. setting.py

--- 07. Task Scheduling/01. Celery With Redis/01. setting.py
+++ 07. Task Scheduling/01. Celery With Redis/01. setting.py
@@ -9,3 +9,11 @@ sudo apt update
 sudo apt install redis-server
 
 # For other systems, refer to the Redis installation guide
+
+
+# Celery configuration for Redis as the broker
+CELERY_BROKER_URL = 'redis://127.0.0.1:6379/0'      # or 'redis://localhost:6379/0'    -> in live server   'redis://domain_or_ip:6379/0'
+CELERY_RESULT_BACKEND = 'redis://127.0.0.1:6379/0'  # or 'redis://localhost:6379/0'    -> in live server   'redis://domain_or_ip:6379/0'  
+CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TASK_SERIALIZER = 'json'
