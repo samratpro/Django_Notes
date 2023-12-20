cc47af2f4cdf4af25e37c39596eef128352e3d64
Samrat Biswas <102436136+samratpro@users.noreply.github.com>
2023-12-20 07:43:35 +0600

    Update 05. task.py

--- 07. Task Scheduling/01. Celery With Redis/05. task.py
+++ 07. Task Scheduling/01. Celery With Redis/05. task.py
@@ -9,6 +9,6 @@ def content_creation_job(arg1, arg2):
 
 
 # After setup or update -> navigate to your Django project directory, and run the Celery worker
->>> celery -A project_name worker -l info   # replace with your project name
+>>> celery -A project_name worker -l INFO   # replace with your project name
 
 
