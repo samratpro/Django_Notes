a702877dbafbf61d77c287b610f81704ee58157f
Samrat Biswas <102436136+samratpro@users.noreply.github.com>
2023-12-20 18:59:46 +0600

    Update 05. task.py

--- 07. Task Scheduling/01. Celery With Redis/05. task.py
+++ 07. Task Scheduling/01. Celery With Redis/05. task.py
@@ -7,6 +7,17 @@ def content_creation_job(arg1, arg2):
     # Task logic goes here
     pass
 
+# example 2
+
+import logging
+logger = logging.getLogger(__name__)
+@shared_task(bind=True)
+def celery_demo_task(self):
+    for i in range(10):
+        sleep(1)
+        logger.info(i)
+    return 'Done'
+
 
 # After setup or update -> navigate to your Django project directory, and run the Celery worker
 >>> celery -A project_name worker -l INFO   # replace with your project name
