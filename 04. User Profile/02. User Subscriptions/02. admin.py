851f64774e6b2d17c14a0f8ad9cbf5d1af8c8465
Samrat Biswas <102436136+samratpro@users.noreply.github.com>
2023-12-28 03:27:10 +0600

    Update 02. admin.py

--- 04. User Profile/02. User Subscriptions/02. admin.py
+++ 04. User Profile/02. User Subscriptions/02. admin.py
@@ -1,1 +1,51 @@
-pass
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import *  # Replace with your custom user model
+
+
+# Custom Admin View for User Management
+# Included password Reset Fields
+class AppUserAdmin(UserAdmin):
+    model = AppUser
+    list_display = ('email', 'first_name', 'last_name', 'is_active', 'is_staff', 'is_superuser')
+    search_fields = ('email', 'first_name', 'last_name')
+    ordering = ('email',)
+    filter_horizontal = ('groups', 'user_permissions')
+
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name')}),
+        ('Permissions', {
+            'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions'),
+        }),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+        ('Custom Fields', {'fields': ('activation_code', 'password_reset_code', 'profile_image', 'credit', 'expire_date', 'credit_package')}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2'),
+        }),
+    )
+
+    # Credit purchase when admin select and save a package with model save override
+    def save_model(self, request, obj, form, change):
+        super().save_model(request, obj, form, change)
+        # Check if a credit package is selected and update user's credit accordingly
+        selected_package = form.cleaned_data.get('credit_package')  # credit_package is 
+        if selected_package:
+            obj.purchase_credit(selected_package)  # This method will purchase credit
+
+    # Check credit expiration checking when admin & staff check any profile
+    def changelist_view(self, request, extra_context=None):
+        # Check credit expiration for all users
+        users = AppUser.objects.all()
+        for user in users:
+            user.credit_expiration()
+        return super().changelist_view(request, extra_context=extra_context)
+
+admin.site.register(AppUser, AppUserAdmin)
+admin.site.register(CreditPackage)
+
+
