84cc2e7eaf28770e14c542d0b75c349481f111da
Samrat Biswas <102436136+samratpro@users.noreply.github.com>
2023-12-28 03:29:13 +0600

    Update 03. views.py

--- 04. User Profile/02. User Subscriptions/03. views.py
+++ 04. User Profile/02. User Subscriptions/03. views.py
@@ -1,1 +1,19 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .models import CreditPackage
 
+
+
+# Purchase credit by user
+@login_required(login_url='login/')
+def purchase_credits(request):
+    if request.method == 'GET':
+        credit_packages = CreditPackage.objects.all()
+        return render(request, 'user/credit/purchase_credits.html', {'credit_packages': credit_packages})
+
+    elif request.method == 'POST':
+        package_id = request.POST.get('package_id')
+        credit_package = CreditPackage.objects.get(pk=package_id)
+        request.user.purchase_credit(credit_package)
+
+        return redirect('profile')
