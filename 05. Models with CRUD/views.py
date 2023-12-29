7cd2b7fb4478e31bbc794770d6a9962bf1fbc0a9
Samrat Biswas <102436136+samratpro@users.noreply.github.com>
2023-12-30 04:45:49 +0600

    Update views.py

--- 05. Models with CRUD/views.py
+++ 05. Models with CRUD/views.py
@@ -160,3 +160,57 @@ def profile(request):
             user_profile.deperment = deparment_instance    # **************** Foreign key take another class's instance so have to pass instance
             user_profile.save()
 
+
+# Uses of next() to retrive gather all similar data that ForignKey connection ***********************************
+# This example is vote count
+# Here has 5 models connection, 1. Vote(), 2. VoteRecord() 3. Department() 4. Semester() 5. AppUser()
+
+@login_required
+def vote_results(request):
+    user = request.user
+    existing_vote = Vote.objects.filter(department=user.department, semester=user.semester).first()
+    if existing_vote:
+        if existing_vote.end_date <= timezone.now():
+            # Annotate votes received by each student for a specific vote
+            vote_records = (
+                VoteRecord.objects
+                .filter(vote=existing_vote)  # Filters VoteRecords by the given existing_vote
+                .values(
+                    'voted_for__id',          # Retrieves the ID of the student voted for
+                    'voted_for__first_name',  # Retrieves the first name of the voted-for student
+                    'voted_for__last_name',   # Retrieves the last name of the voted-for student
+                    'voted_for__profile_image' # Retrieves the profile image of the voted-for student
+                )
+                .annotate(total_votes=Count('voted_for'))  # Counts the total votes received by each voted-for student
+            )
+            
+            # Get all students in the department and semester
+            all_students = AppUser.objects.filter(semester=user.semester, department=user.department)
+            
+            # Combine both vote records and students who haven't received votes
+            # Adding 'total_votes' to all students to indicate votes received
+            for student in all_students:
+                student.total_votes = next((record['total_votes'] for record in vote_records if record['voted_for__id'] == student.id), 0)
+            '''
+                1. `student.total_votes` for assigns a new attribute to each student object. 
+                    It adds dynamically to the student object retrieved from all_students.
+
+                2. student: Represents each student in the loop.
+
+                3. record['total_votes'] for record in vote_records if record['voted_for__id'] == student.id: 
+                   This part is a generator expression. 
+                   It iterates through vote_records and filters to find records where the voted_for user ID matches 
+                   the current student.id. It retrieves the total_votes for that student if such a record is found.
+
+                4. filters the vote_records queryset to find the records where the voted_for 
+                   user ID matches the current student.id. It retrieves the total_votes for that student if found.
+
+                5. next(): This function retrieves the next value from the generator expression. 
+
+                6. If no matching record is found (i.e., the student hasn't received any votes), it returns a default value of 0.
+            '''     
+            return render(request, 'vote/vote_results.html', {'all_students': all_students})
+        else:
+            return render(request, 'vote/vote_running.html', {'existing_vote': existing_vote})
+    else:
+        return render(request, 'vote/no_vote_results.html')
