3fb45111369a5468b41daf5ced5db1c2ceec58da
Samrat Biswas <102436136+samratpro@users.noreply.github.com>
2023-12-29 07:59:56 +0600

    Update 04. urls.py

--- 04. User Profile/02. User Subscriptions/04. urls.py
+++ 04. User Profile/02. User Subscriptions/04. urls.py
@@ -1,1 +1,14 @@
+# URL Mapping in App URL file----------------------
+from django.urls import path
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
+
+urlpatterns = [
+
+    path('purchase-credits/', views.purchase_credits, name='purchase_credits'),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
