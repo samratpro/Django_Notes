ac7d142fafd4ddd09560308b8975c5a9ef1539cf
Samrat Biswas <102436136+samratpro@users.noreply.github.com>
2024-01-19 05:31:50 +0600

    Update 03. serializers.py

--- 08. REST Framework/02. Session Base Authentication from Jinja (fetch - ajax)/03. serializers.py
+++ 08. REST Framework/02. Session Base Authentication from Jinja (fetch - ajax)/03. serializers.py
@@ -6,3 +6,13 @@ from rest_framework import serializers
 
 class GenerateTextSerializer(serializers.Serializer):
     input_text = serializers.CharField(max_length=1000)
+
+
+class Info_Bulk_Posting_Serializer(serializers.Serializer):
+    keyword_list = serializers.CharField()
+    website_id = serializers.CharField(max_length=500)
+    youtubeapi_id = serializers.CharField(max_length=500, required=False, allow_blank=True)
+    category = serializers.CharField(max_length=500, required=False, allow_blank=True)
+    post_status = serializers.CharField(max_length=200)
+    img_status = serializers.CharField(max_length=200)
+
