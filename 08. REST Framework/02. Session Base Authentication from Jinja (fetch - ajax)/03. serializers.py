3b60e6ef7fa20a011fbc8d754c5c12db1c3e5006
Samrat Biswas <102436136+samratpro@users.noreply.github.com>
2024-01-18 14:50:33 +0600

    Update 03. serializers.py

--- 08. REST Framework/02. Session Base Authentication from Jinja (fetch - ajax)/03. serializers.py
+++ 08. REST Framework/02. Session Base Authentication from Jinja (fetch - ajax)/03. serializers.py
@@ -1,5 +1,7 @@
 # Important part for API serialization
 
+# https://www.django-rest-framework.org/api-guide/fields/
+
 from rest_framework import serializers
 
 class GenerateTextSerializer(serializers.Serializer):
