7458079de9caa4abeed27ade9d95847fd1e43b36
Samrat Biswas <102436136+samratpro@users.noreply.github.com>
2024-01-19 14:55:42 +0600

    Update 02. setting.py

--- 02. setting.py
+++ 02. setting.py
@@ -43,3 +43,46 @@ DATABASES = {
         'HOST': 'localhost',
     }
 }
+
+
+
+# Django Logger ---- ** It is most important when application move in server
+# path - Need to create ` logs ` folder and `console.log` file in folder
+# logs / console.log  
+
+import os
+from pathlib import Path
+from django.conf import settings
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+        },
+        "file": {
+            "level": "DEBUG",
+            "class": "logging.FileHandler",
+            "filename": str(Path(settings.BASE_DIR) / 'logs' / 'console.log'),
+        },
+    },
+    "root": {
+        "handlers": ["console", "file"],
+        "level": "WARNING",
+    },
+    "loggers": {
+        "django": {
+            "handlers": ["console", "file"],
+            "level": os.getenv("DJANGO_LOG_LEVEL", "INFO"),
+            "propagate": True,
+        },
+    },
+}
+
+
+# in views.py or any other files
+import logging
+logger = logging.getLogger("django")
+logger.info('Message')
+
