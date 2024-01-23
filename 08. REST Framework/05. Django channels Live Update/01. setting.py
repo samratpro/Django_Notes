d3588bcfd7fa69c0d17fd6463189c37a54421d5c
Samrat Biswas <102436136+samratpro@users.noreply.github.com>
2024-01-23 06:09:07 +0600

    Update 01. setting.py

--- 08. REST Framework/05. Django channels Live Update/01. setting.py
+++ 08. REST Framework/05. Django channels Live Update/01. setting.py
@@ -1,4 +1,5 @@
 # pip install channels
+# pip install channels_api
 
 # Install app in setting
 
@@ -7,3 +8,14 @@ INSTALLED_APPS = [
     'channels',
 ]
 
+
+# Add channels middleware
+MIDDLEWARE = [
+    # ...
+    'channels.middleware.WebSocketMiddleware',
+]
+
+# Configure channels-allowed-hosts
+CHANNELS_API = {
+    "ALLOWED_HOSTS": ["*"],
+}
