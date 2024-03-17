af08b6f6fc88592e8081e35ff936ae4c577033b6
Samrat Biswas <102436136+samratpro@users.noreply.github.com>
2024-03-18 00:23:40 +0600

    Update 02. admin.py

--- 03. User Authentication/01. Email Verification with AbstractUser/02. admin.py
+++ 03. User Authentication/01. Email Verification with AbstractUser/02. admin.py
@@ -15,25 +15,25 @@ admin.site.site_header = 'AI Writing Project'
 from django.contrib.auth.admin import UserAdmin
 class AppUserAdmin(UserAdmin):
     model = AppUser
-    list_display = ('email', 'first_name', 'last_name', 'is_active', 'is_staff', 'is_superuser')
-    search_fields = ('email', 'first_name', 'last_name')
+    list_display = ('email', 'first_name', 'last_name', 'is_active', 'is_staff', 'is_superuser',)
+    search_fields = ('email', 'first_name', 'last_name',)
     ordering = ('email',)
-    filter_horizontal = ('groups', 'user_permissions')
+    filter_horizontal = ('groups', 'user_permissions',)
 
     fieldsets = (
         (None, {'fields': ('email', 'password')}),
         ('Personal info', {'fields': ('first_name', 'last_name')}),
         ('Permissions', {
-            'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions'),
+            'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions',),
         }),
-        ('Important dates', {'fields': ('last_login', 'date_joined')}),
-        ('Custom Fields', {'fields': ('activation_code', 'password_reset_code', 'profile_image', 'credit')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined',)}),
+        ('Custom Fields', {'fields': ('activation_code', 'password_reset_code', 'profile_image', 'credit',)}),
     )
 
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
-            'fields': ('email', 'password1', 'password2'),
+            'fields': ('email', 'password1', 'password2',),
         }),
     )
 
